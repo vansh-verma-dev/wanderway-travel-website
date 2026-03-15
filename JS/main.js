ff481968ac7ea35c0b620eed18665cac644a2250
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-15 19:06:37 +0530

    Added travel mode cards section

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,20 @@ const navLinks = document.getElementById("navLinks");
 menuBtn.addEventListener("click", () => {
 navLinks.classList.toggle("active");
 });
+
+
+ const container = document.querySelector(".travel-cards");
+
+let scrollAmount = 0;
+
+setInterval(() => {
+
+scrollAmount += 1;
+
+container.scrollLeft = scrollAmount;
+
+if(scrollAmount >= container.scrollWidth - container.clientWidth){
+scrollAmount = 0;
+}
+
+}, 20);
