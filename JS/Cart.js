2f245adc456378d3fa546c462615264d7413d849
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-25 03:14:07 +0700

    initila add css file for cart page desgin

--- JS/Cart.js
+++ JS/Cart.js
@@ -1,24 +1,30 @@
 // container select karo
 let Trip_Collection = document.querySelector('.trip_collect');
-
+ 
 // function to load data
 function showTrips() {
   let trips = JSON.parse(localStorage.getItem("trips")) || [];
-
-  // console check
-  console.log(trips);
-
-  // clear old data
-  Trip_Collection.innerHTML = "";
-
-  // agar koi trip nahi hai
+ 
+  // agar koi trip nahi hai to ye wala data loard hoga
   if (trips.length === 0) {
-    Trip_Collection.innerHTML = `<a href="index.html#Destination"><button>Explore</button>
-        </a>`;
+    Trip_Collection.innerHTML = ` 
+      <div class="cart_content">
+        <span class="icon">
+          <i class="fa-solid fa-cart-arrow-down"></i>
+          <h2>Your Cart is Empty</h2>
+          <p>Start adding destinations to plan your perfect trip!</p>
+          <a href="index.html">
+            <button>
+              Explore Destinations<i
+                class="fa-solid fa-arrow-right"
+              ></i></button
+          ></a>
+        </span>
+      </div>
+    `;
     return;
-  }
-
-  // loop chalao
+  }else{  
+ 
   trips.forEach((trip, index) => {
     Trip_Collection.innerHTML += `
     <div class="trip_bill">
@@ -41,18 +47,18 @@ function showTrips() {
                 <h2  >₹${trip.price || "N/A"}</h2>
             </div>
 
-            <div class="trip_btn_group">
+        </div>
+          <div class="trip_btn_group">
                 <button class="confrmBtn">Confirm Booking</button>
                 <button class="delete_btn">Cancel Trip</button>
                 <button class="RemoveBtn" onclick="deleteTrip(${index})">
                   Remove
                 </button>
             </div>
-
-        </div>
     </div>
     `;
   });
+}
 }
 
 // delete function
@@ -66,5 +72,5 @@ function deleteTrip(index) {
   showTrips(); 
 }
 
-// page load pe run
+// page load  run
 window.onload = showTrips;
